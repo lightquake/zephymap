39db1127d4e65965635b6e2ea72ac7b27db6693a
Patrick Hurst <phurst@mit.edu>
2010-01-17 05:58:29 -0500

    new, sexier parser. only returns headers, and will return duplicates for multi-folder messages, though

--- emailparser.py
+++ emailparser.py
@@ -26,6 +26,7 @@ class EmailParser:
         """
         date_header = get_field("Date", mail_string)
         date_tuple = email.utils.parsedate_tz(date_header)
+        #timezone fuckery due to parsedate not interpreting other timezones properly
         time_received = calendar.timegm(date_tuple[0:8]) - date_tuple[9]
         return time_received > self.last_check
     
@@ -37,26 +38,21 @@ class EmailParser:
         Return the number of unread messages.
         """
         folders = map(lambda x: x.split(' ')[2].strip('"'), self.imap.list()[1])
-        notifications = []
+        headers = []
         for folder in folders:
             if "[Gmail]" in folder: continue
             if self.imap.select(folder, True)[0] == "NO": continue # open read-only
-            response, unseen = self.imap.search(None, 'UNSEEN')
-            indices = unseen[0].split()
-            nmessages = len(indices)
-            i = nmessages - 1
-            while i >= 0:
-                # Fetch the received date and remove the preceding 'Date: '
-                rfc2822 = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (DATE)])')[1][0][1][6:]
-                time_received = time.mktime(email.utils.parsedate(rfc2822))
-                if time_received + 10 * 60 < self.last_check:
-                    break
-                sender = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (FROM)])')[1][0][1][6:-4]
-                subject = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (SUBJECT)])')[1][0][1][9:-4]
-                notifications.append({"sender" : sender, "subject" : subject})
-                i -= 1
+            throwaway, unseen = self.imap.search(None, 'UNSEEN')
+            if unseen == ['']: continue
+            indices = ','.join(unseen[0].split())
+            print folder
+            # for some reason, I get )s mixed in with actual header/response pair information.
+            headers = [x[1] for x in self.imap.fetch(indices, "(BODY[HEADER.FIELDS (DATE FROM SUBJECT)])")[1] if x != ')']
+            new_headers = filter(self.is_new, headers)
+            headers += new_headers
+            
         self.last_check = time.time()
-        return notifications
+        return headers
 
 def get_field(name, field_string):
     fields = filter(lambda x: x.startswith(name.capitalize() + ":"), field_string.split("\r\n"))
