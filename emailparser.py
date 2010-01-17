da9309ab29d6fe3b3ae2585c8af02aa83c9a3a86
Patrick Hurst <phurst@mit.edu>
2010-01-17 02:58:09 -0500

    make emailparser.py multi-mailbox aware

--- emailparser.py
+++ emailparser.py
@@ -19,22 +19,25 @@ class EmailParser:
         Check for messages received since the last check.
         Return the number of unread messages.
         """
-        folders = map(lambda x: x.split(' ')[2].strip('"'), server.list()[1])
-        self.imap.select()
-        response, unseen = self.imap.search(None, 'UNSEEN')
-        indices = unseen[0].split()
-        nmessages = len(indices)
-        i = nmessages - 1
+        folders = map(lambda x: x.split(' ')[2].strip('"'), self.imap.list()[1])
         notifications = []
-        while i >= 0:
-            # Fetch the received date and remove the preceding 'Date: '
-            rfc2822 = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (DATE)])')[1][0][1][6:]
-            time_received = time.mktime(email.utils.parsedate(rfc2822))
-            if time_received < self.last_check:
-                break
-            sender = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (FROM)])')[1][0][1][6:-4]
-            subject = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (SUBJECT)])')[1][0][1][9:-4]
-            notifications.append({"sender" : sender, "subject" : subject})
-            i -= 1
+        for folder in folders:
+            if "[Gmail]" in folder: continue
+            if self.imap.select(folder, True)[0] == "NO": continue # open read-only
+            response, unseen = self.imap.search(None, 'UNSEEN')
+            indices = unseen[0].split()
+            nmessages = len(indices)
+            i = nmessages - 1
+            
+            while i >= 0:
+                # Fetch the received date and remove the preceding 'Date: '
+                rfc2822 = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (DATE)])')[1][0][1][6:]
+                time_received = time.mktime(email.utils.parsedate(rfc2822))
+                if time_received + 10 * 60 < self.last_check:
+                    break
+                sender = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (FROM)])')[1][0][1][6:-4]
+                subject = self.imap.fetch(indices[i], '(BODY[HEADER.FIELDS (SUBJECT)])')[1][0][1][9:-4]
+                notifications.append({"sender" : sender, "subject" : subject})
+                i -= 1
         self.last_check = time.time()
         return notifications
